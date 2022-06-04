15744bcbc9e23cb5416b3654f3c5cc57ee59df55
Eason Chen <arx827@gmail.com>
2022-06-04 21:48:16 +0800

    deploy

---
+++ assets/index.html.46d51595.js
@@ -0,0 +1,29 @@
+import{_ as t,r as c,o,c as r,a as e,w as a,b as n,d as s,e as d}from"./app.f15ed2f5.js";const u={},m=n("h3",{id:"\u50CF\u6578-1-2-3-\u4E00\u6A23\u5BB9\u6613",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u50CF\u6578-1-2-3-\u4E00\u6A23\u5BB9\u6613","aria-hidden":"true"},"#"),s(" \u50CF\u6578 1, 2, 3 \u4E00\u6A23\u5BB9\u6613")],-1),p=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# \u5728\u4F60\u7684\u9805\u76EE\u4E2D\u5B89\u88DD"),s(`
+`),n("span",{class:"token function"},"yarn"),s(),n("span",{class:"token function"},"add"),s(` -D vuepress@next
+
+`),n("span",{class:"token comment"},"# \u65B0\u5EFA\u4E00\u500B markdown \u6587\u4EF6"),s(`
+`),n("span",{class:"token builtin class-name"},"echo"),s(),n("span",{class:"token string"},"'# Hello VuePress'"),s(),n("span",{class:"token operator"},">"),s(` README.md
+
+`),n("span",{class:"token comment"},"# \u958B\u59CB\u5BEB\u4F5C"),s(`
+`),n("span",{class:"token function"},"yarn"),s(` vuepress dev
+
+`),n("span",{class:"token comment"},"# \u69CB\u5EFA\u975C\u614B\u6587\u4EF6"),s(`
+`),n("span",{class:"token function"},"yarn"),s(` vuepress build
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),v=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# \u5728\u4F60\u7684\u9805\u76EE\u4E2D\u5B89\u88DD"),s(`
+`),n("span",{class:"token function"},"npm"),s(),n("span",{class:"token function"},"install"),s(` -D vuepress@next
+
+`),n("span",{class:"token comment"},"# \u65B0\u5EFA\u4E00\u500B markdown \u6587\u4EF6"),s(`
+`),n("span",{class:"token builtin class-name"},"echo"),s(),n("span",{class:"token string"},"'# Hello VuePress'"),s(),n("span",{class:"token operator"},">"),s(` README.md
+
+`),n("span",{class:"token comment"},"# \u958B\u59CB\u5BEB\u4F5C"),s(`
+npx vuepress dev
+
+`),n("span",{class:"token comment"},"# \u69CB\u5EFA\u975C\u614B\u6587\u4EF6"),s(`
+npx vuepress build
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),b=d(`<div class="language-markdown ext-md line-numbers-mode"><pre class="language-markdown"><code><span class="token comment">&lt;!-- \u9ED8\u8BA4\u60C5\u51B5\u4E0B\uFF0C\u8FD9\u91CC\u4F1A\u88AB\u4FDD\u6301\u539F\u6837 --&gt;</span>
+1 + 2 + 3 = {{ 1 + 2 + 3 }}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-markdown ext-md line-numbers-mode"><pre class="language-markdown"><code><span class="token comment">&lt;!-- \u8FD9\u91CC\u4F1A\u88AB Vue \u7F16\u8BD1 --&gt;</span>
+1 + 2 + 3 = 6
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code><span class="token comment">// \u7531\u4E8E JS \u4EE3\u7801\u9AD8\u4EAE\uFF0C\u8FD9\u91CC\u4E0D\u4F1A\u88AB\u6B63\u786E\u7F16\u8BD1</span>
+<span class="token keyword">const</span> onePlusTwoPlusThree <span class="token operator">=</span> <span class="token punctuation">{</span><span class="token punctuation">{</span> <span class="token number">1</span> <span class="token operator">+</span> <span class="token number">2</span> <span class="token operator">+</span> <span class="token number">3</span> <span class="token punctuation">}</span><span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div>`,3);function k(h,_){const l=c("CodeGroupItem"),i=c("CodeGroup");return o(),r("div",null,[m,e(i,null,{default:a(()=>[e(l,{title:"YARN",active:""},{default:a(()=>[p]),_:1}),e(l,{title:"NPM"},{default:a(()=>[v]),_:1})]),_:1}),b])}var f=t(u,[["render",k],["__file","index.html.vue"]]);export{f as default};
