05eba2875db8e89565314930fed743c60b3e3d2f
Eason Chen <arx827@gmail.com>
2022-06-04 21:26:57 +0800

    [feature] 『init』

--- docs/.vuepress/configs/navbar.ts
+++ docs/.vuepress/configs/navbar.ts
@@ -6,42 +6,42 @@ export const navbar: NavbarConfig = [
     text: '指南',
     link: '/guide/',
   },
-  {
-    text: '参考',
-    children: [
-      {
-        text: 'VuePress',
-        children: [
-          '/reference/cli.md',
-          '/reference/config.md',
-          '/reference/frontmatter.md',
-          '/reference/components.md',
-          '/reference/plugin-api.md',
-          '/reference/theme-api.md',
-          '/reference/client-api.md',
-          '/reference/node-api.md',
-        ],
-      },
-      {
-        text: '打包工具',
-        children: [
-          '/reference/bundler/vite.md',
-          '/reference/bundler/webpack.md',
-        ],
-      },
-      {
-        text: '默认主题',
-        children: [
-          '/reference/default-theme/config.md',
-          '/reference/default-theme/frontmatter.md',
-          '/reference/default-theme/components.md',
-          '/reference/default-theme/markdown.md',
-          '/reference/default-theme/styles.md',
-          '/reference/default-theme/extending.md',
-        ],
-      },
-    ],
-  },
+  // {
+  //   text: '参考',
+  //   children: [
+  //     {
+  //       text: 'VuePress',
+  //       children: [
+  //         '/reference/cli.md',
+  //         '/reference/config.md',
+  //         '/reference/frontmatter.md',
+  //         '/reference/components.md',
+  //         '/reference/plugin-api.md',
+  //         '/reference/theme-api.md',
+  //         '/reference/client-api.md',
+  //         '/reference/node-api.md',
+  //       ],
+  //     },
+  //     {
+  //       text: '打包工具',
+  //       children: [
+  //         '/reference/bundler/vite.md',
+  //         '/reference/bundler/webpack.md',
+  //       ],
+  //     },
+  //     {
+  //       text: '默认主题',
+  //       children: [
+  //         '/reference/default-theme/config.md',
+  //         '/reference/default-theme/frontmatter.md',
+  //         '/reference/default-theme/components.md',
+  //         '/reference/default-theme/markdown.md',
+  //         '/reference/default-theme/styles.md',
+  //         '/reference/default-theme/extending.md',
+  //       ],
+  //     },
+  //   ],
+  // },
   // {
   //   text: '插件',
   //   children: [
